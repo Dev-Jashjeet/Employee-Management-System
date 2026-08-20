e6ceac787eb943ddee43d9dedf35d86cbbdb77f7
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-21 01:05:18 +0530

    makes folder structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
+import 'remixicon/fonts/remixicon.css'
+
+function App() {
+
+  return(
+    <>
+    <Toaster />
+    </>
+  )
+}
