969d1149262053ffa27b99eb2eb6d56cb5d04831
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-23 23:25:53 +0530

    Refactor App component to use default export

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
 import { Toaster } from 'react-hot-toast'
 import 'remixicon/fonts/remixicon.css'
+import './App.css'
+import Login from './components/auth/Login'
+import EmployeeDashboard from './components/dashboard/EmployeeDashboard'
 
-function App() {
+export default function App() {
 
   return(
     <>
+    {/* <Login /> */}
+    <EmployeeDashboard />
     <Toaster />
     </>
   )
