4cc43da8d3a9118e4278eed7eabdc458062af8ac
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-26 23:59:01 +0530

    admin dashboard all task file uploaded

---
+++ src/components/others/AllTasks.tsx
@@ -0,0 +1,32 @@
+export default function AllTasks() {
+
+    return(
+        <div id="scrollBar" className="bg-gray-800 h-70 w-full p-5 mt-10 overflow-y-auto flex flex-col gap-3">
+            <div className="flex justify-between p-3 text-white text-xl bg-red-400 rounded-lg">
+                <span>Sarthak</span>
+                <span>Make a UI Design</span>
+                <span>Status</span>
+            </div>
+            <div className="flex justify-between p-3 text-white text-xl bg-green-400 rounded-lg">
+                <span>Sarthak</span>
+                <span>Make a UI Design</span>
+                <span>Status</span>
+            </div>
+            <div className="flex justify-between p-3 text-white text-xl bg-yellow-400 rounded-lg">
+                <span>Sarthak</span>
+                <span>Make a UI Design</span>
+                <span>Status</span>
+            </div>
+            <div className="flex justify-between p-3 text-white text-xl bg-blue-400 rounded-lg">
+                <span>Sarthak</span>
+                <span>Make a UI Design</span>
+                <span>Status</span>
+            </div>
+            <div className="flex justify-between p-3 text-white text-xl bg-red-400 rounded-lg">
+                <span>Sarthak</span>
+                <span>Make a UI Design</span>
+                <span>Status</span>
+            </div>
+        </div>
+    )
+}
