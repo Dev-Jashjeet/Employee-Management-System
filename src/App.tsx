e4ed2716fddcf8e2d5cbc9de5e079038475f403a
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-27 00:00:17 +0530

    admin dashboard UI buils

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import 'remixicon/fonts/remixicon.css'
 import './App.css'
 import Login from './components/auth/Login'
 import EmployeeDashboard from './components/dashboard/EmployeeDashboard'
+import AdminDashboard from './components/dashboard/AdminDashboard'
 
 export default function App() {
 
   return(
     <>
     {/* <Login /> */}
-    <EmployeeDashboard />
+    {/* <EmployeeDashboard /> */}
+    <AdminDashboard />
     <Toaster />
     </>
   )
