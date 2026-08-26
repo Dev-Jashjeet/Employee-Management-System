34ef5ce67e171d03b77180d0f29c6732920e37d9
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-26 23:57:10 +0530

    admin dashboard UI build

---
+++ src/components/dashboard/AdminDashboard.tsx
@@ -0,0 +1,14 @@
+import AllTasks from "../others/AllTasks";
+import CreateTask from "../others/CreateTask";
+import Header from "../others/Header";
+
+export default function AdminDashboard() {
+
+    return(
+        <div className="w-full h-screen bg-gray-900 p-10">
+            <Header />
+            <CreateTask />
+            <AllTasks />
+        </div>
+    )
+}
