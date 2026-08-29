d1638ce70aa68f9e6fab49b1af1c289ec311c015
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-30 00:34:33 +0530

    Refactor EmployeeDashboard to use employee data prop
    Updated EmployeeDashboard to accept employee data as a prop and pass it to child components.

--- src/components/dashboard/EmployeeDashboard.tsx
+++ src/components/dashboard/EmployeeDashboard.tsx
@@ -1,16 +1,16 @@
+import type { employeeType } from "../../types/allFilesTypes";
 import Header from "../others/Header";
 import TaskNumbers from "../others/TaskNumbers";
-import TaskLists from "../tasklists/TasksList";
+import TaskLists from "../tasklists/TasksLists";
 
-export default function EmployeeDashboard() {
-
+export default function EmployeeDashboard({data}: {data: employeeType}) {
     return(
         <div className="h-screen bg-gray-800 p-10">
-            <Header />
+            <Header data={data} />
 
-            <TaskNumbers />
+            <TaskNumbers data={data} />
 
-            <TaskLists />
+            <TaskLists data={data} />
         </div>
     )
 }
