912858fa3476f17394225dad9beecda9e7fbef3b
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-30 00:42:07 +0530

    Refactor TaskLists to accept employee data as props

--- src/components/tasklists/TasksList.tsx
+++ src/components/tasklists/TasksList.tsx
@@ -1,16 +1,31 @@
+import type { employeeType, taskType } from "../../types/allFilesTypes";
 import AcceptTask from "./AcceptTask";
 import CompleteTask from "./CompleteTask";
 import { FailedTask } from "./FailedTask";
 import NewTask from "./NewTask";
 
-export default function TaskLists() {
-
+export default function TaskLists({data}: {data: employeeType}) {
+    if(!data) {
+        return;
+    }
     return(
         <div id="scrollBar" className="w-full mt-14 flex gap-5 overflow-x-auto">
-            <FailedTask />
-            <NewTask />
-            <AcceptTask />
-            <CompleteTask />
+            {
+                data.tasks.map((user: taskType, index: number) => {
+                    if(user.active) {
+                        return <AcceptTask key={index} data={user} />
+                    }
+                    else if(user.completed) {
+                        return <CompleteTask key={index} data={user} />
+                    }
+                    else if(user.failed) {
+                        return <FailedTask key={index} data={user} />
+                    }
+                    else {
+                        return <NewTask key={index} data={user} />
+                    }
+                })
+            }
         </div>
     )
 }
