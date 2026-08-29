7070b81b06fc7781f02625845c4a8aafb70dfa7e
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-30 00:34:06 +0530

    Pass undefined data prop to Header in AdminDashboard
    Added 'data' prop to Header component in AdminDashboard.

--- src/components/dashboard/AdminDashboard.tsx
+++ src/components/dashboard/AdminDashboard.tsx
@@ -6,7 +6,7 @@ export default function AdminDashboard() {
 
     return(
         <div className="w-full h-screen bg-gray-900 p-10">
-            <Header />
+            <Header data={undefined} />
             <CreateTask />
             <AllTasks />
         </div>
