943c11ad8d1d13390992e4674c6c73ced298f978
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-30 00:37:05 +0530

    Update Header component to accept employee data

--- src/components/others/Header.tsx
+++ src/components/others/Header.tsx
@@ -1,10 +1,11 @@
-export default function Header() {
+import type { employeeType } from "../../types/allFilesTypes";
 
+export default function Header({data}: {data: employeeType|undefined}) {
     return(
         <header className="w-full flex justify-between items-center">
             <div className="text-white">
                 <h1 className="text-3xl font-medium">Hello,</h1>
-                <h1 className="text-4xl font-bold">Sarthak 👋</h1>
+                <h1 className="text-4xl font-bold">{data? data.firstName: "Admin"} 👋</h1>
             </div>
             <button className="bg-red-500 text-white font-bold p-3 pr-5 pl-5 rounded-xl cursor-pointer transition-all hover:bg-red-600">Logout</button>
         </header>
