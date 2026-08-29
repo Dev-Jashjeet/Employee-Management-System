8b20a953a04fcb4a6065239e870bce99b1669751
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-30 00:40:01 +0530

    Refactor CompleteTask component to accept props

--- src/components/tasklists/CompleteTask.tsx
+++ src/components/tasklists/CompleteTask.tsx
@@ -1,16 +1,21 @@
-export default function CompleteTask() {
+import type { taskType } from "../../types/allFilesTypes";
+
+export default function CompleteTask({data}: {data: taskType}) {
+    if(!data) {
+        return;
+    }
 
     return(
         <div className="bg-green-500 shrink-0 p-5 pb-10 text-white w-100 rounded-xl h-fit flex flex-col gap-5">
             <div className="w-full flex justify-between items-center">
                 <span className="bg-red-500 rounded-lg p-2 pr-3 pl-3">High</span>
-                <span>{new Date().toLocaleDateString()}</span>
+                <span>{data.taskDate}</span>
             </div>
             <div className="text-2xl font-bold">
-                Make a Youtube Video
+                {data.taskTitle}
             </div>
             <div className="text-lg">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi distinctio illum ducimus maiores magnam eum doloremque. Veritatis, perspiciatis magnam consectetur deserunt eveniet saepe, incidunt dolore alias nisi, voluptates eos repudiandae?
+                {data.taskDescription}
             </div>
         </div>
     )
