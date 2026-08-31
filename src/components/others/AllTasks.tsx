34d3548364ee1866fb27f032cbb1b0002d848bd2
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-31 19:39:18 +0530

    Refactor AllTasks component to use auth data

--- src/components/others/AllTasks.tsx
+++ src/components/others/AllTasks.tsx
@@ -1,32 +1,21 @@
+import { useContext } from "react"
+import { AuthContext } from "../../context/AuthProvider"
+import type { employeeType, getLocalStorageType } from "../../types/allFilesTypes";
+
 export default function AllTasks() {
+    const authData: undefined|getLocalStorageType = useContext(AuthContext);
 
     return(
         <div id="scrollBar" className="bg-gray-800 h-70 w-full p-5 mt-10 overflow-y-auto flex flex-col gap-3">
-            <div className="flex justify-between p-3 text-white text-xl bg-red-400 rounded-lg">
-                <span>Sarthak</span>
-                <span>Make a UI Design</span>
-                <span>Status</span>
-            </div>
-            <div className="flex justify-between p-3 text-white text-xl bg-green-400 rounded-lg">
-                <span>Sarthak</span>
-                <span>Make a UI Design</span>
-                <span>Status</span>
-            </div>
-            <div className="flex justify-between p-3 text-white text-xl bg-yellow-400 rounded-lg">
-                <span>Sarthak</span>
-                <span>Make a UI Design</span>
-                <span>Status</span>
-            </div>
-            <div className="flex justify-between p-3 text-white text-xl bg-blue-400 rounded-lg">
-                <span>Sarthak</span>
-                <span>Make a UI Design</span>
-                <span>Status</span>
-            </div>
-            <div className="flex justify-between p-3 text-white text-xl bg-red-400 rounded-lg">
-                <span>Sarthak</span>
-                <span>Make a UI Design</span>
-                <span>Status</span>
-            </div>
+            {
+                authData && authData.employees.map((user: employeeType, index: number) => (
+                    <div key={index} className="flex justify-between p-3 text-white text-xl border border-green-400 rounded-lg">
+                        <span>{user.firstName}</span>
+                        <span>{user.email}</span>
+                        <span>{user.taskCounts.active}</span>
+                    </div>       
+                ))
+            }
         </div>
     )
 }
