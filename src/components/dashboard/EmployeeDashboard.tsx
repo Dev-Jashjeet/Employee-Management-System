2e84582ef5e3e6a614292a02eced1c03520b0c73
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-31 19:38:40 +0530

    Add setUser prop to EmployeeDashboard component

--- src/components/dashboard/EmployeeDashboard.tsx
+++ src/components/dashboard/EmployeeDashboard.tsx
@@ -3,10 +3,10 @@ import Header from "../others/Header";
 import TaskNumbers from "../others/TaskNumbers";
 import TaskLists from "../tasklists/TasksLists";
 
-export default function EmployeeDashboard({data}: {data: employeeType}) {
+export default function EmployeeDashboard({data, setUser}: {data: employeeType, setUser: Function}) {
     return(
         <div className="h-screen bg-gray-800 p-10">
-            <Header data={data} />
+            <Header setUser={setUser} data={data} />
 
             <TaskNumbers data={data} />
 
