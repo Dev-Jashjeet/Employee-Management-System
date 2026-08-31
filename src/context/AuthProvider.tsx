4c1ec744a21e8e2230838916e79daa751a866295
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-31 19:41:50 +0530

    Refactor AuthProvider to simplify useEffect
    Removed unnecessary localStorage setting in useEffect.

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,19 +1,17 @@
 import { createContext, useEffect, useState } from "react"
-import { getLocalStorage, setLocalStorage } from "../utils/localStorage";
+import { getLocalStorage } from "../utils/localStorage";
 import type { getLocalStorageType } from "../types/allFilesTypes";
 
 export const AuthContext = createContext<undefined|getLocalStorageType>(undefined)
 
 const AuthProvider = ({children}: {children: React.ReactNode}) => {
-    const [userData, setUserData] = useState<getLocalStorageType|undefined>(undefined);
-
-    useEffect((): void => {
-      setLocalStorage();
-        const data: getLocalStorageType|undefined = getLocalStorage();
-        if(!data) return;
-        setUserData(data);
-    }, [])
-
+  const [userData, setUserData] = useState<getLocalStorageType|undefined>(undefined);
+  useEffect((): void => {
+    const data: getLocalStorageType|undefined = getLocalStorage();
+    if(!data) return;
+    setUserData(data);
+  }, []);
+  
   return (
     <div>
         <AuthContext.Provider value={userData}>
