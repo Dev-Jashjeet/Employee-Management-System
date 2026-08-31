c1ec7d72f4acce3f1f051599a50a0defc585abfd
Jashjeet Singh <jashjeet99@gmail.com>
2026-08-31 19:38:19 +0530

    Add setUser prop to AdminDashboard component

--- src/components/dashboard/AdminDashboard.tsx
+++ src/components/dashboard/AdminDashboard.tsx
@@ -2,11 +2,11 @@ import AllTasks from "../others/AllTasks";
 import CreateTask from "../others/CreateTask";
 import Header from "../others/Header";
 
-export default function AdminDashboard() {
+export default function AdminDashboard({setUser}: {setUser: Function}) {
 
     return(
         <div className="w-full h-screen bg-gray-900 p-10">
-            <Header data={undefined} />
+            <Header setUser={setUser} data={undefined} />
             <CreateTask />
             <AllTasks />
         </div>
